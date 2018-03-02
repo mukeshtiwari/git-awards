c082456876bd6122a18553f01400420e1511b7ab
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-03-02 16:06:45 +1100

    small script to download git-award user list. I have written to answer Quora question :)

---
+++ git-awards-download.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(require net/url
+         html-parsing
+         sxml/sxpath)
+
+(define (page-get url)
+  (call/input-url (string->url url)
+                  get-pure-port
+                  html->xexp))
+
+(define (parse-user-name ul)
+  (match (last ul)
+    [(cons _ (cons _ (cons c _))) c]
+    [_ '()]))
+
+(define (filter-values re)
+  (match re
+    [(cons a (cons b (cons c (cons d res))))
+     (cons (string-join (list (parse-user-name b) (last c) (first (rest d))) ",") (filter-values res))]
+    [_ '()]))
+
+(define (list-of-values url)
+  (let* ([res (page-get url)]
+         [ret ((sxpath '(// tbody tr td)) res)]
+         [val (filter-values ret)])
+    val))
+
+(define res (list-of-values "http://git-awards.com/users?utf8=%E2%9C%93&type=city&language=swift&city=Paris"))
+(define h (list-of-values "http://git-awards.com/users"))
